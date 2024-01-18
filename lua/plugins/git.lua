aca5abd2b3f36067f4eca8dcc396c0e0a162cb71
Boyd Bloemsma <boydbloemsma@gmail.com>
2024-01-18 23:05:51 +0100

    Added styling and fixed intelephense

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -2,7 +2,17 @@ return {
 	{
         "lewis6991/gitsigns.nvim",
         config = function ()
-            require("gitsigns").setup()
+            require("gitsigns").setup({
+                signs = {
+                    add = { text = 'A' },
+                    change = { text = 'M' },
+                    delete = { text = 'D' },
+                    topdelete = { text = 'TD' },
+                    changedelete = { text = 'CD' },
+                    untracked = { text = 'U' },
+                },
+                numhl = true,
+            })
         end
     },
     "tpope/vim-fugitive",
