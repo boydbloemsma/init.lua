d99873cf35a7488cdcd037790af70d0101a5a9fa
Boyd Bloemsma <boydbloemsma@gmail.com>
2024-01-20 16:53:20 +0100

    Added lualine

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -4,4 +4,16 @@ return {
       cmd = "IncRename",
       config = true,
     },
+    "tpope/vim-sleuth",
+    {
+        "nvim-lualine/lualine.nvim",
+        opts = {
+            options = {
+                icons_enabled = false,
+                theme = 'gruvbox',
+                component_separators = '|',
+                section_separators = '',
+            }
+        }
+    }
 }
